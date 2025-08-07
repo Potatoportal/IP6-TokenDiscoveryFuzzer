a77acfbef0043dce0b2f3b7580ad939a26e8afb3
hoangviet-nguyen <viety.nguyen02@gmail.com>
2025-08-07 20:55:42 +0200

    removed test stage

--- libfuzzer_libmozjpeg/src/lib.rs
+++ libfuzzer_libmozjpeg/src/lib.rs
@@ -3,8 +3,6 @@
 use core::time::Duration;
 use std::{collections::{HashMap, HashSet}, env, path::PathBuf};
 
-mod test_stage;
-
 use libafl::{
     corpus::{Corpus, InMemoryCorpus, OnDiskCorpus},
     events::{setup_restarting_mgr_std, EventConfig, EventRestarter},
@@ -31,38 +29,13 @@ use libafl_bolts::{
     serdeany::RegistryBuilder, 
     tuples::{tuple_list, Merge}, 
     AsSlice, 
-    SerdeAny,
-
 };
 
 use libafl_targets::{libfuzzer_initialize, libfuzzer_test_one_input, EDGES_MAP, MAX_EDGES_FOUND};
 use mimalloc::MiMalloc;
-use serde::{Serialize, Deserialize};
 
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
-
-#[derive(Debug, Serialize, Deserialize, SerdeAny)]
-pub struct SeenTestCases {
-    tescases: HashSet<Vec<u8>>,
-    index_to_values: HashMap<usize, HashSet<u8>>,
-}
-
-impl SeenTestCases
-where {
-
-    pub fn new () -> Self {
-        Self { tescases: HashSet::new(), index_to_values: HashMap::new()}
-    }
-
-    pub fn testcases_mut(&mut self) -> &mut HashSet<Vec<u8>> {
-        &mut self.tescases
-    }
-
-    pub fn index_to_value_map_mut(&mut self) -> &mut HashMap<usize, HashSet<u8>> {
-        &mut self.index_to_values
-    }
-}
 
 
 /// The main fn, `no_mangle` as it is a C main
